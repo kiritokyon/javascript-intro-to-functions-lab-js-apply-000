8cb6c0ec390d3426b1e6d815da024ffa4f297e8f
eager-strategy-8796 <cheniasli@gmail.com>
2019-06-05 14:19:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   
-  if (logWhisper(string) === true) {
+  if (whsiper(string) === true) {
     
    return "I can't hear you!" ;
    
