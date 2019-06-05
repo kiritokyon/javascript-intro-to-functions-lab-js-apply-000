6cce4d55d84bf481db8b981503f8af5a2b34a509
eager-strategy-8796 <cheniasli@gmail.com>
2019-06-05 14:08:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function shout(string) {
   return string.toUpperCase();
 }
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+
+
+
+
+
+function sayHiToGrandma(string) {
+  return 
+}
